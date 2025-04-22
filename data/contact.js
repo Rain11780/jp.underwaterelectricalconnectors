4e7c729a841ac6ab957a113a683df3aae9c47410
Rain11780 <nikowu18@gmail.com>
2025-04-22 10:31:54 +0800

    2

--- data/contact.js
+++ data/contact.js
@@ -1,24 +1,24 @@
 export const contact = {
 	header: {
-		title: "Contact Us",
-		focus: "Contact",
-		description: "Connect with HYSF for custom IP68 underwater electrical connectors. Our team supports your marine, deep-sea, and freshwater projects with tailored solutions. Reach out today to discuss your needs and get started!"
+		title: "お問い合わせ",
+		focus: "お問い合わせ",
+		description: "HYSFと連絡を取り、カスタマイズされたIP68水中電気コネクタについてご相談ください。当社のチームは、海洋、深海、淡水プロジェクトをカスタムソリューションでサポートします。今日、ニーズについて話し合い、スタートしましょう！"
 	},
 	contact_info: [
 		{
-			label: "Make a Call",
+			label: "電話をかける",
 			description: "",
 			value: "+852 6147 0250"
 		},
 		{
-			label: "Send an Email",
+			label: "メールを送信",
 			description: "",
 			value: "info@underwaterelectricalconnectors.com"		
 		},
 		{
-			label: "Address",
+			label: "住所",
 			description: "",
-			value: "1-1207, Building A1#, Shengde Garden, No. 133 Yonghua Road, Anci District, Langfang City, Hebei Province, China"
+			value: "中国河北省廊坊市安次区永華路133番地、盛徳ガーデンA1号棟1-1207"
 		}
 	],
 	map_link: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3078.488103857386!2d116.68737892091033!3d39.50347205252088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35f1db51d6b60b7f%3A0xc97e0161f74001a6!2z55ub5b636Iqx5Zut77yI5YyX6Zeo77yJ!5e0!3m2!1szh-CN!2shk!4v1744874123590!5m2!1szh-CN!2shk"
